b965bb0abaf888b91fee7ab133d8ffcfa3fa8ef2
010101aa <155654553+010101aa@users.noreply.github.com>
2024-02-01 10:08:01 +0100

    Add files via upload

---
+++ install_macos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/010101aa/pac_man.git
+
+mv pac_man ~/Desktop/
+
+python3 ~/Desktop/pac_man/pac_man_python/main.py
+
+chmod 755 ~/Desktop/pac_man/run.sh
+
+# Beenden Sie das Skript
+exit 0
